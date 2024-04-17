5646577aadd902fbaaf2f02329987b4d20d74beb
Grzysztof <3384940+Grzysztof@users.noreply.github.com>
2024-04-17 23:37:24 +0200

    feat: test experiments

--- pytainer/pytainer.py
+++ pytainer/pytainer.py
@@ -4,19 +4,16 @@ import os
 from urllib.parse import urljoin
 
 class Pytainer:
-    _requester: httpx.Client
     _base_url: str
     _headers: dict
     _api_token: str
 
-    def __init__(self, * , portainer_adsr: str | None) -> None:
-        if not portainer_adsr:
+    def __init__(self, * , base_url: str | None) -> None:
+        if not base_url:
             self._base_url = os.getenv('PORTAINER_URL')
         else:
-            self._base_url = portainer_adsr
+            self._base_url = base_url
         
-
-        self._requester = httpx.Client(base_url=self._base_url)
         self._headers = {}
 
         self.stack = Stack(self)
@@ -26,22 +23,25 @@ class Pytainer:
 class APIResource:
     _client: Pytainer
 
+    def __init__(self, client: Pytainer) -> None:
+        self.client = client
+
 # IMPL
 class Stack(APIResource):
+    _base_path = '/stacks'
+
     def get(self) -> dict:
-        test = self.client.get('/todos')
+        test = httpx.get(urljoin(self.client._base_url, self._base_path))
         print(test.json())
         return test.json()
 
 
 class Auth(APIResource):
+    _base_path = '/auth'
+
     def __call__(self, login: str | None) -> dict:
         self.headers = {"X-Client-ID": "ABC123"}
-        auth_req = self.client.get('/auth')
+        auth_req = httpx.get(urljoin(self.client._base_url, self._base_path))
+        self.client._api_token = auth_req.json()['jwt']
         print(auth_req.json())
         return(auth_req.json())
-
-if __name__=="__main__":
-    client = Pytainer(url='https://mocki.io/')
-    print(dir(client))
-    print(vars(client))
--- tests/test_pytainer.py
+++ tests/test_pytainer.py
@@ -1,28 +1,24 @@
 import pytest
+import httpx
+from pytest_httpx import HTTPXMock
+
 from pytainer.pytainer import Pytainer
 
-@pytest.fixture
-def mock_api():
-    with httpx_mock.Mocker() as m:
-        yield m
 
-@pytest.fixture
-def user_repository(mock_api):
-    base_url = "https://api.example.com"
-    repo = UserRepository(base_url)
-    return repo
-@pytest.fixture
-def api_client():
-    # Initialize your API client with appropriate configuration
-    client = Pytainer(url='https://portaienr.free.beeceptor.com')
-    return client
+def test_get_stacks(httpx_mock: HTTPXMock):
+    httpx_mock.add_response(url="https://portainer.test/stacks", json={"resp": "test"})
+    portainer = Pytainer(base_url="https://portainer.test")
 
-# Test case to check if the API client can retrieve data
-def test_api_get_data(api_client):
-    data = api_client.stack.get()
-    assert data is not None
-    assert data == {"resp": "api"}
+    with httpx.Client() as client:
+        resp = portainer.stack.get()
+        assert resp == {"resp": "test"}
 
-def test_api_auth(api_client):
-    data = api_client.auth(login='test')
-    assert data == {"resp": "test"}
+def test_auth(httpx_mock: HTTPXMock):
+    login = "test-login"
+    httpx_mock.add_response(url="https://portainer.test/auth", json={"jwt": login})
+    portainer = Pytainer(base_url="https://portainer.test")
+
+    with httpx.Client() as client:
+        resp = portainer.auth(login)
+        assert portainer._api_token == login
+        assert resp == {"jwt": login}
