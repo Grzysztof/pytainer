7e82cae4c2c8301f81efc27ab7d51c736bdb33f7
Grzysztof <3384940+Grzysztof@users.noreply.github.com>
2024-04-24 22:52:26 +0200

    feat: add check if token/base url are present. add stack start/stop cli commands and methods. Add minor tests

--- pytainer/__init__.py
+++ pytainer/__init__.py
@@ -25,19 +25,19 @@ class Pytainer:
 
     def __init__(
         self,
-        base_url: str | None,
+        base_url: str | None = None,
         api_token: str | None = None,
         username: str | None = None,
         password: str | None = None,
     ) -> None:
-        if not base_url:
-            self.base_url = os.getenv("PORTAINER_URL")
-        else:
-            self.base_url = base_url
-        if not api_token:
-            self.api_token = os.getenv("PORTAINER_API_TOKEN")
-        else:
-            self.api_token = api_token
+        self._base_url = base_url or os.getenv("PORTAINER_URL")
+        self._api_token = api_token or os.getenv("PORTAINER_API_TOKEN")
+        
+        if not self._api_token:
+            raise Exception("API token is required")
+        
+        if not self._base_url:
+            raise Exception("Base URL is required")
 
         self._headers = {}
         self._requester = httpx.Client()
@@ -45,34 +45,10 @@ class Pytainer:
         self.stacks = Stacks(self)
         self.auth = Auth(self)
         self.system = System(self)
-
-    @property
-    def headers(self) -> dict:
-        return self._headers
-
-    @headers.setter
-    def headers(self, headers: dict) -> None:
-        self._headers.update(headers)
-
-    @property
-    def api_token(self) -> str:
-        return self._api_token
-
-    @api_token.setter
-    def api_token(self, token: str) -> None:
-        self._api_token = token
-
+    
     @property
     def base_url(self) -> str:
         return self._base_url
-
-    @base_url.setter
-    def base_url(self, url: str) -> None:
-        self._base_url = url
-
-    @property
-    def requester(self) -> httpx.Client:
-        return self._requester
 
     def make_request(
         self,
@@ -81,14 +57,15 @@ class Pytainer:
         params: dict | None = None,
         data: BaseModel | None = None,
     ) -> httpx.Response:
-        self.headers["X-API-Key"] = f"{self.api_token}"
+        
+        self._headers["X-API-Key"] = f"{self._api_token}"
 
         if data:
             data = data.model_dump_json()
-
-        return self._requester.request(
-            method, url, data=data, headers=self.headers, params=params
-        )
+        req = self._requester.request(
+            method, url, data=data, headers=self._headers, params=params
+        ).raise_for_status()
+        return req
 
 
 class APIResource:
@@ -208,41 +185,63 @@ class Stacks(APIResource):
     _client: Pytainer
 
     def list(self) -> List[portainer.Stack]:
-        """List stacks"""
+        """List stacks https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackList"""
         req = self.client.make_request(
-            "GET", urljoin(self.client._base_url, self._base_path)
+            "GET", urljoin(self.client.base_url, self._base_path)
         )
-        print(req.json())
         return [portainer.Stack.model_validate(stack_item) for stack_item in req.json()]
 
     def update(
         self, stack_id: int, endpoint_id: int, data: portainer.UpdateSwarmStackPayload
     ) -> dict:
-        """Update a stack"""
-        req = self.client.make_request(
-            "PUT",
-            urljoin(self.client._base_url, f"{self._base_path}/{stack_id}"),
-            params={"endpointId": endpoint_id},
-            data=data,
-        )
-        return portainer.Stack.model_validate(req.json())
+        """Update a stack https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackUpdate"""
+        try:
+            req = self.client.make_request(
+                HttpMethod.PUT,
+                urljoin(self.client.base_url, f"{self._base_path}/{stack_id}"),
+                params={"endpointId": endpoint_id},
+                data=data,
+            ).raise_for_status()
+            return portainer.Stack.model_validate(req.json())
+        except httpx.RequestError as e:
+            raise Exception(f"An error occurred while updating the stack: {e}")
 
     def get(self, stack_id: int) -> portainer.Stack:
-        """Get a stack"""
-        req = self.client.make_request(
-            "GET",
-            urljoin(self.client._base_url, f"{self._base_path}/{stack_id}"),
-        )
-        return portainer.Stack.model_validate(req.json())
+        """Get a stack https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackInspect"""
+        try:
+            req = self.client.make_request(
+                HttpMethod.GET,
+                urljoin(self.client.base_url, f"{self._base_path}/{stack_id}"),
+            ).raise_for_status()
+            return portainer.Stack.model_validate(req.json())
+        except httpx.RequestError as e:
+            raise Exception(
+                f"An error occurred while getting the stack: {e}, {req.json()}"
+            )
 
     def get_file(self, stack_id: int) -> portainer.Stack:
-        """Get stack file"""
+        """Get stack file https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackFileInspect"""
         req = self.client.make_request(
-            "GET",
-            urljoin(self.client._base_url, f"{self._base_path}/{stack_id}/file"),
+            HttpMethod.GET,
+            urljoin(self.client.base_url, f"{self._base_path}/{stack_id}/file"),
         )
         return portainer.StackFileResponse.model_validate(req.json())
 
+    def start(self, stack_id: int, endpoint_id: int) -> portainer.Stack:
+        """Start a stack https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackStart"""
+        req = self.client.make_request(
+            HttpMethod.POST,
+            urljoin(self.client.base_url, f"{self._base_path}/{stack_id}/start"), params={"endpointId": endpoint_id}
+        )
+        return portainer.Stack.model_validate(req.json())
+    
+    def stop(self, stack_id: int, endpoint_id: int) -> portainer.Stack:
+        """Stop a stack https://app.swaggerhub.com/apis/portainer/portainer-ce/2.19.4#/stacks/StackStop"""
+        req = self.client.make_request(
+            HttpMethod.POST,
+            urljoin(self.client.base_url, f"{self._base_path}/{stack_id}/stop"), params={"endpointId": endpoint_id}
+        )
+        return portainer.Stack.model_validate(req.json())
 
 class Status(APIResource):
     pass
@@ -250,18 +249,14 @@ class Status(APIResource):
 
 class System(APIResource):
     def info(self) -> portainer.SystemInfoResponse:
-        resource_path = "api/system/info"
-        request_url = urljoin(self.client.base_url, resource_path)
-        info_req = self.client.make_request(HttpMethod.GET, request_url)
-        info_resp = portainer.SystemInfoResponse.model_validate(info_req.json())
-        return info_resp
+        """Get system info"""
+        info_req = self.client.make_request(HttpMethod.GET, urljoin(self.client.base_url, "api/system/info"))
+        return portainer.SystemInfoResponse.model_validate(info_req.json())
 
     def version(self) -> portainer.SystemVersionResponse:
-        resource_path = "api/system/version"
-        request_url = urljoin(self.client.base_url, resource_path)
-        version_req = self.client.make_request(HttpMethod.GET, request_url)
-        version_resp = portainer.SystemInfoResponse.model_validate(version_req.json())
-        return version_resp
+        """Get Portainer version"""
+        info_req = self.client.make_request(HttpMethod.GET, urljoin(self.client.base_url, "api/system/version"))
+        return portainer.SystemVersionResponse.model_validate(info_req.json())
 
 
 class Tags(APIResource):
